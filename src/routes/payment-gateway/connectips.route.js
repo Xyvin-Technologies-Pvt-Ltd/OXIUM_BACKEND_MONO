79632fe92c406df4f6d1d95d54ef8f365dfce277
Sarath Chandran <sarathc.dev@gmail.com>
2025-11-12 11:41:19 +0530

    change connectips route

--- src/routes/payment-gateway/connectips.route.js
+++ src/routes/payment-gateway/connectips.route.js
@@ -6,15 +6,13 @@ router.post("/payment/connectips/initiate", connectipsController.initiatePayment
 router.get("/payment/connectips/success", connectipsController.paymentSuccess);
 router.get("/payment/connectips/failure", connectipsController.paymentFailure);
 
-
-
 // const connectipsRoute = require("express").Router();
 // const connectIpsController = require("../../controllers/connectIpsController");
 
 // Payment initiation
 // connectipsRoute.post("/initiate", connectIpsController.initiatePayment);
 
-// module.exports = router;
+module.exports = router;
 // Payment callbacks
 // connectipsRoute.get("/success", connectIpsController.handleSuccess);
 // connectipsRoute.get("/failure", connectIpsController.handleFailure);
@@ -23,4 +21,4 @@ router.get("/payment/connectips/failure", connectipsController.paymentFailure);
 // connectipsRoute.post("/verify/:txnId", connectIpsController.verifyPayment);
 // connectipsRoute.get("/status/:txnId", connectIpsController.getPaymentStatus);
 
-module.exports = connectipsRoute;
+// module.exports = connectipsRoute;
