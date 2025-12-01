516007529ac07ad543cf3df91b044cde5566eefa
Sarath Chandran <sarathc.dev@gmail.com>
2025-12-01 13:48:13 +0530

    chore(connectips): remove unnecessary comments in route definitions

--- src/routes/payment-gateway/connectips.route.js
+++ src/routes/payment-gateway/connectips.route.js
@@ -8,21 +8,4 @@ router.get("/payment/connectips/failure", connectipsController.paymentFailure);
 router.get("/payment/connectips/status/:txnId", connectipsController.checkPaymentStatus);
 
 
-
 module.exports = router;
-
-// const connectipsRoute = require("express").Router();
-// const connectIpsController = require("../../controllers/connectIpsController"); 
-
-// Payment initiation
-// connectipsRoute.post("/initiate", connectIpsController.initiatePayment);
-
-// Payment callbacks
-// connectipsRoute.get("/success", connectIpsController.handleSuccess);
-// connectipsRoute.get("/failure", connectIpsController.handleFailure);
-
-// Payment verification
-// connectipsRoute.post("/verify/:txnId", connectIpsController.verifyPayment);
-// connectipsRoute.get("/status/:txnId", connectIpsController.getPaymentStatus);
-
-// module.exports = connectipsRoute;
