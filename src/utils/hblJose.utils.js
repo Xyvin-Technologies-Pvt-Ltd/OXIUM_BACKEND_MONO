78c3e550862d266aed31be7157afa5588a3febc9
Sarath Chandran <sarathc.dev@gmail.com>
2026-09-25 16:22:23 +0530

    refactor(paymentGateway): update HBL configuration to use production environment variables
    Replaced UAT environment variables with production counterparts for HBL keys and configuration settings. This change ensures the application uses the correct keys for production deployment, enhancing security and operational integrity.

--- src/utils/hblJose.utils.js
+++ src/utils/hblJose.utils.js
@@ -29,19 +29,19 @@ const loadKeys = async () => {
 
   return {
     merchantSigningPrivateKey: await importPKCS8(
-      formatPemKey(process.env.HBL_UAT_MERCHANT_SIGNING_PRIVATE_KEY, 'MERCHANT_SIGNING_PRIVATE_KEY'), 
+      formatPemKey(process.env.HBL_MERCHANT_SIGNING_PRIVATE_KEY, 'HBL_MERCHANT_SIGNING_PRIVATE_KEY'),
       'PS256'
     ),
     merchantDecryptionPrivateKey: await importPKCS8(
-      formatPemKey(process.env.HBL_UAT_MERCHANT_DECRYPTION_PRIVATE_KEY, 'MERCHANT_DECRUPTION_PRIVATE_KEY'),
+      formatPemKey(process.env.HBL_MERCHANT_DECRYPTION_PRIVATE_KEY, 'HBL_MERCHANT_DECRYPTION_PRIVATE_KEY'),
       'RSA-OAEP'
     ),
     pacoEncryptionPublicKey: await importSPKI(
-      formatPemKey(process.env.HBL_UAT_PACO_ENCRYPTION_PUBLIC_KEY, 'PACO_ENCRYPTION_PUBLIC_KEY'),
+      formatPemKey(process.env.HBL_PACO_ENCRYPTION_PUBLIC_KEY, 'HBL_PACO_ENCRYPTION_PUBLIC_KEY'),
       'RSA-OAEP'
     ),
     pacoSigningPublicKey: await importSPKI(
-      formatPemKey(process.env.HBL_UAT_PACO_SIGNING_PUBLIC_KEY, 'PACO_SIGNING_PUBLIC_KEY'),
+      formatPemKey(process.env.HBL_PACO_SIGNING_PUBLIC_KEY, 'HBL_PACO_SIGNING_PUBLIC_KEY'),
       'PS256'
     )
   };
@@ -54,10 +54,10 @@ const initializeKeys = async () => {
 };
 
 const getHblConfig = () => ({
-  baseUrl: process.env.HBL_UAT_BASE_URL,
-  keyId: process.env.HBL_UAT_KEY_ID,
-  apiKey: process.env.HBL_UAT_API_KEY,
-  merchantId: process.env.HBL_UAT_MERCHANT_ID,
+  baseUrl: process.env.HBL_BASE_URL,
+  keyId: process.env.HBL_KEY_ID,
+  apiKey: process.env.HBL_API_KEY,
+  merchantId: process.env.HBL_MERCHANT_ID,
   successUrl: process.env.HBL_SUCCESS_URL,
   failUrl: process.env.HBL_FAILURE_URL
 });
@@ -100,7 +100,7 @@ const createJosePayload = async (payload, clientIp = '1.0.0.1') => {
     storeCardDetails: { storeCardFlag: "N", storedCardUniqueID: "{{guid}}" },
     installmentPaymentDetails: { ippFlag: "N", installmentPeriod: 0, interestType: null },
     mcpFlag: "N",
-    request3dsFlag: "N",
+    request3dsFlag: "Y",
     transactionAmount: {
       amountText: String(Math.round(payload.amount * 100)).padStart(12, '0'),
       currencyCode: "NPR",
